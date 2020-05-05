3872c32975c4f95eb70206cfa15d81a5c8dfa005
RyanMCprog <54039851+RyanMCprog@users.noreply.github.com>
2020-05-04 17:25:19 -0700

    LevelManager obstacleUi tilescript

---
+++ Bundemic/Assets/SafeSpace/scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject tile;
+    public int MapLength;
+    public int MapWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void createLevel()
+    {
+        
+        for(int z = 0; z < MapLength; z++)
+        {
+            for(int x = 0; x < MapWidth; x++)
+            {
+                GameObject newTile = Instantiate(tile);
+                newTile.transform.position = new Vector3(-MapLength/2 + 0.5f + x, 0, -MapWidth / 2 + 0.5f + z);
+            }
+        }
+    }
+}
